b71ac2f0775645f88acb6d67461215d04bb99d34
zachvem <matej.zachveja@softec.sk>
2026-08-16 21:55:35 +0200

    sanitizer

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,10 @@
 #include "file_stats.h"
 
 #include <iostream>
+#define STRSIZE 256
 
-int main(int argc, char* argv[]) {
-  std::cout << "Hello world!" << std::endl;
-  return 0;
+int global_array[100] = {-1};
+int main(int argc, char* argv[])
+{
+    return global_array[100 + 100];
 }
