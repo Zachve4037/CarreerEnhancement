68bf55ad65a08ee28901a3e3872127d9e6db3ddc
Zachve4037 <matej.zachveja@gmail.com>
2026-08-17 10:43:21 +0200

    CMakeLists.txt update sanitizers

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,8 @@
 #include "file_stats.h"
 
 #include <iostream>
-#define STRSIZE 256
 
-int global_array[100] = {-1};
-int main(int argc, char* argv[])
+int main()
 {
-    return global_array[100 + 100];
+    char* str = new char[5];
 }
