c857f046b253c03a178b65aa3bc23461585c10f1
Zachve4037 <matej.zachveja@gmail.com>
2026-09-17 19:34:50 +0200

    comments for pipeline

--- src/6_week_process_supervisor/process_supervisor.h
+++ src/6_week_process_supervisor/process_supervisor.h
@@ -1,25 +1,25 @@
-#ifndef CPPFIRST_PROCESS_SUPERVISOR_H
-#define CPPFIRST_PROCESS_SUPERVISOR_H
-
-#include <string>
-#include <vector>
-
-enum class ProcessStatus {
-  exited,
-  signaled,
-  failed_to_start
-};
-
-struct ProcessResult {
-  ProcessStatus status;
-  int exit_code{-1};
-  int signal{-1};
-  std::string output;
-};
-
-class ProcessSupervisor {
-public:
-  ProcessResult run(const std::vector<std::string>& command) const;
-};
-
-#endif
+// #ifndef CPPFIRST_PROCESS_SUPERVISOR_H
+// #define CPPFIRST_PROCESS_SUPERVISOR_H
+//
+// #include <string>
+// #include <vector>
+//
+// enum class ProcessStatus {
+//   exited,
+//   signaled,
+//   failed_to_start
+// };
+//
+// struct ProcessResult {
+//   ProcessStatus status;
+//   int exit_code{-1};
+//   int signal{-1};
+//   std::string output;
+// };
+//
+// class ProcessSupervisor {
+// public:
+//   ProcessResult run(const std::vector<std::string>& command) const;
+// };
+//
+// #endif
