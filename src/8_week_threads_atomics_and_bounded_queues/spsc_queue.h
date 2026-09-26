8820df62346c5cc80970241941f5affc81082d5a
Zachve4037 <matej.zachveja@gmail.com>
2026-09-26 10:39:56 +0200

    spsc_queue implementation

--- src/8_week_threads_atomics_and_bounded_queues/spsc_queue.h
+++ src/8_week_threads_atomics_and_bounded_queues/spsc_queue.h
@@ -7,27 +7,49 @@
 #include <atomic>
 #include <cstddef>
 #include <memory>
+#include <stdexcept>
 
-template <typename T>
-class SPSCQueue {
+template <typename T> class SPSCQueue {
 public:
-  explicit SPSCQueue(std::size_t capacity);
+  explicit SPSCQueue(std::size_t capacity)
+      : capacity_(capacity == 0
+                      ? throw std::invalid_argument("capacity cannot be 0")
+                      : capacity),
+        buffer_(std::make_unique<T[]>(capacity_)){}
 
-  //returns false if queue is full
-  bool try_push(const T& item);
+  // returns false if queue is full
+  bool try_push(const T &item) {
+    const std::size_t tail = tail_.load(std::memory_order_acquire);
+    const std::size_t head = head_.load(std::memory_order_relaxed);
+    if (head - tail == capacity_) {
+      return false;
+    }
+    buffer_[head % capacity_] = item;
+    head_.store(head + 1, std::memory_order_release);
+    return true;
+  }
 
-  //returns false if the queue is empty
-  bool try_pop(T& item);
+  // returns false if the queue is empty
+  bool try_pop(T &item) {
+    const std::size_t tail = tail_.load(std::memory_order_relaxed);
+    const std::size_t head = head_.load(std::memory_order_acquire);
+    if (head == tail) {
+      return false;
+    }
+    item = std::move(buffer_[tail % capacity_]);
+    tail_.store(tail + 1, std::memory_order_release);
+    return true;
+  }
 
-  std::size_t capacity() const noexcept;
+  std::size_t capacity() const noexcept { return capacity_; }
 
 private:
   const std::size_t capacity_;
   std::unique_ptr<T[]> buffer_;
 
-  //producer writes to the head
+  // producer writes to the head
   std::atomic<std::size_t> head_{0};
-  //consumer writes to the tail
-  std::atomic<std::size_t> tail{0};
+  // consumer writes to the tail
+  std::atomic<std::size_t> tail_{0};
 };
 #endif // CPPFIRST_SPSC_QUEUE_H
--- test/spsc_queue_test.cpp
+++ test/spsc_queue_test.cpp
@@ -1,3 +1,21 @@
 //
 // Created by zachvem on 24-Sep-26.
 //
+#include "../src/8_week_threads_atomics_and_bounded_queues/spsc_queue.h"
+#include "gtest/gtest.h"
+
+TEST(spsc_queue_test, RejectsZeroCapacity) {
+  EXPECT_THROW(SPSCQueue<int>(0), std::invalid_argument);
+}
+
+TEST(spsc_queue_test, StorecCapacity) {
+  SPSCQueue<int> queue(8);
+  EXPECT_EQ(queue.capacity(), 8);
+}
+
+//empty queue
+//basic push/pop
+//full queue
+//wraparound - pop some items push more verify FIFO order
+//spsc concurrency - one producer and one consumer transfer
+//  many sequential integers without loss or reordering
