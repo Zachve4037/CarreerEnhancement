326618c8376a5304c50cdfcb5a14b522d064ee96
Zachve4037 <matej.zachveja@gmail.com>
2026-09-26 23:07:09 +0200

    queue_demo implementation

--- src/8_week_threads_atomics_and_bounded_queues/queue_demo.cpp
+++ src/8_week_threads_atomics_and_bounded_queues/queue_demo.cpp
@@ -1,3 +1,20 @@
-//
-// Created by zachvem on 24-Sep-26.
-//
+#include "bounded_blocking_queue.h"
+
+#include <iostream>
+#include <thread>
+
+int main() {
+ BoundedBlockingQueue<int> queue(5);
+ std::thread producer([&]() {
+  for (int i = 0; i < 10; ++i) {
+   queue.push(i);
+  }
+ });
+ std::thread consumer([&]() {
+  for (int i = 0; i < 10; ++i) {
+   std::cout << queue.pop() << std::endl;
+  }
+ });
+ producer.join();
+ consumer.join();
+}
